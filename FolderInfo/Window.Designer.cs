d5e30c57d599d20622778474354891ce9632ee57
fernandoespinosa <fernandoespinosa@fernandoespinosa.org>
2016-07-07 11:50:52 -0400

    Initial commit.

---
+++ FolderInfo/Window.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace FolderInfo
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxRootPath = new System.Windows.Forms.TextBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxRootPath
+            // 
+            this.textBoxRootPath.Location = new System.Drawing.Point(12, 12);
+            this.textBoxRootPath.Name = "textBoxRootPath";
+            this.textBoxRootPath.Size = new System.Drawing.Size(616, 20);
+            this.textBoxRootPath.TabIndex = 0;
+            this.textBoxRootPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxRootPath_KeyDown);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(12, 38);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(616, 280);
+            this.dataGrid.TabIndex = 1;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 330);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.textBoxRootPath);
+            this.Name = "Window";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxRootPath;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
+
---
+++ FolderInfo/Window.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FolderInfo
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxRootPath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.dataGrid.DataSource = GetFolderInfos(this.textBoxRootPath.Text);
+            }
+        }
+
+        private object GetFolderInfos(string rootPath)
+        {
+            var rootDir = new DirectoryInfo(rootPath);
+            return rootDir.EnumerateDirectories().Select(d => new { d.Name, Size = GetFullSize(d) }).ToList();
+        }
+
+        private FolderSize GetFullSize(DirectoryInfo dir)
+        {
+            return dir.EnumerateFiles("*.*", SearchOption.AllDirectories).Sum(f => f.Length);
+        }
+    }
+
+    public struct FolderSize
+    {
+        private long _size;
+        private FolderSize(long size)
+        {
+            _size = size;
+        }
+        public override string ToString()
+        {
+            return _size < 1024 ? string.Format("{0:N0} bytes", _size)
+                : _size < 1024 * 1024 ? string.Format("{0:N} KB", _size / 1024d)
+                : _size < 1024 * 1024 * 1024 ? string.Format("{0:N} MB", _size / (1024 * 1024d))
+                : string.Format("{0:N} GB", _size / (1024 * 1024 * 1024d));
+        }
+        public static implicit operator FolderSize(long size) { return new FolderSize(size); }
+    }
+}
